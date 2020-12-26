3cf4e4f458e64acaeaa626c8e6fdadc85e4ac4af
Бруйко Евгения <bruyko.e@gmail.com>
2020-12-27 00:30:51 +0300

    Initial Commit

---
+++ Step By Step/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Step By Step
+//
+//  Created by Brubrusha on 12/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Step By Step/Step_By_StepApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_By_StepApp.swift
+//  Step By Step
+//
+//  Created by Brubrusha on 12/27/20.
+//
+
+import SwiftUI
+
+@main
+struct Step_By_StepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
